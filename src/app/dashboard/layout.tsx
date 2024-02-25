a204a10342ee278a4c36bfd933eee52c04b54a15
Web Dev Cody <webdevcody@gmail.com>
2024-02-24 19:31:44 -0500

    fix full screen layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="container mx-auto pt-12">
+    <main className="container mx-auto pt-12 min-h-screen">
       <div className="flex gap-8">
         <SideNav />
 
